822d7147ee629515ec92ae03bf4a764f6c672244
Avishka Kavinda <45445179+acekavi@users.noreply.github.com>
2021-12-27 18:01:54 +0530

    Completed the layout of the landing page

---
+++ js/mainscript.js
@@ -0,0 +1,9 @@
+$(document).ready(()=>{
+//   $("button[data-mdb-toggle=collapse]").click(function() {
+//     if($("button[aria-controls=flus-collapseOne]").hasClass("collapsed")){
+//       $("button[aria-controls=flush-collapseOne]").removeClass("fav");
+//     }else{
+//       $("button[aria-controls=flush-collapseOne]").addClass("fav");
+//     }
+//   });
+});
