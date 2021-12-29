339b96a15283af3a70b572d3c04061fcbee4b9ed
Avishka Kavinda <45445179+acekavi@users.noreply.github.com>
2021-12-29 10:18:44 +0530

    minified the project files and added local scripts for ui and jquery

--- js/mainscript.js
+++ js/mainscript.js
@@ -3,122 +3,55 @@ fetch("shoes.json")
    return response.json();
 })
 .then(data => {
-  data.shoes.slice(-4).forEach(element => {
-    $(".hot-picks").append(
-    '<div class="card p-0 m-2">'
-        +'<div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">'
-          +'<img src="/img/'+element.id+'/1.jpg" class="card-img" />'
-          +'<a href="#!">'
-            +'<div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>'
-          +'</a>'
-        +'</div>'
-        +'<div class="card-header d-flex justify-content-around">'
-        +'<h5 class="my-auto">Hot Picks 🔥</h5>'
-          +'<div class="drag">'
-          +'<a class="btn btn-danger btn-floating">'
-            +'<i class="fas fa-heart"></i>'
-          +'</a></div>'
-        +'</div>'
-        +'<div class="card-body">'
-          +'<h5 class="card-title">'+element.name+'</h5>'
-          +'<p class="card-text m-0 p-0">For '+element.gender+'</p>'
-          +'<p class="card-text m-0 p-0">Colour : '+element.colour+'</p>'
-          +'<p class="card-text">Price : $'+element.price+'</p>'
-        +'</div>'
-      +'</div>'
-  )
-  });
-  $( ".drag" ).draggable({
-    revert: true,
-    helper: () => {
-      return '<button type="button" class="btn btn-danger btn-floating dragged">'
-              +'<i class="fas fa-heart" data-mdb-toggle="tooltip" data-mdb-placement="bottom" title="Add to favorites"></i>'
-            +'</button>';
+  data.shoes.forEach((element, index, arr) => {
+    if(index >= arr.length - 4){
+      $(".section-hotPicks").append(generateCard(element));
     }
-  });
-});
 
-fetch("shoes.json")
-.then(response => {
-   return response.json();
-})
-.then(data => {
-  data.shoes.forEach(element => {
     if(element.gender === "Men"){
-      $(".section-men").append(
-        '<div class="card p-0 m-2 '+element.id+'">'
-          +'<div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">'
-            +'<img src="/img/'+element.id+'/1.jpg" class="card-img" />'
-            +'<a href="#!">'
-              +'<div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>'
-            +'</a>'
-          +'</div>'
-          +'<div class="card-body">'
-            +'<h5 class="card-title">'+element.name+'</h5>'
-            +'<div class="colourList lh-sm"></div>'
-            +'<p class="card-text">Price : $'+element.price+'</p>'
-            +'<button type="button" class="btn btn-danger btn-floating">'
-              +'<i class="fas fa-heart" data-mdb-toggle="tooltip" data-mdb-placement="bottom" title="Add to favorites"></i>'
-            +'</button>'
-          +'</div>'
-        +'</div>'
-      )
+      $(".section-men").append(generateCard(element));
     }
 
     else if(element.gender === "Women"){
-      $(".section-women").append(
-        '<div class="card p-0 m-2 '+element.id+'">'
-          +'<div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">'
-            +'<img src="/img/'+element.id+'/1.jpg" class="card-img"/>'
-            +'<a href="#!">'
-              +'<div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>'
-            +'</a>'
-          +'</div>'
-          +'<div class="card-body">'
-            +'<h5 class="card-title">'+element.name+'</h5>'
-            +'<div class="colourList"></div>'
-            +'<p class="card-text">Price : $'+element.price+'</p>'
-            +'<button type="button" class="btn btn-danger btn-floating">'
-              +'<i class="fas fa-heart" data-mdb-toggle="tooltip" data-mdb-placement="bottom" title="Add to favorites"></i>'
-            +'</button>'
-          +'</div>'
-        +'</div>'
-      );
+      $(".section-women").append(generateCard(element))
     }
 
     else{
-      $(".section-kids").append(
-        '<div class="card p-0 m-2 '+element.id+'">'
-          +'<div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">'
-            +'<img src="/img/'+element.id+'/1.jpg" class="card-img" />'
-            +'<a href="#!">'
-              +'<div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>'
-            +'</a>'
-          +'</div>'
-          +'<div class="card-body">'
-            +'<h5 class="card-title">'+element.name+'</h5>'
-            +'<div class="colourList"></div>'
-            +'<p class="card-text">Price : $'+element.price+'</p>'
-            +'<button type="button" class="btn btn-danger btn-floating">'
-              +'<i class="fas fa-heart" data-mdb-toggle="tooltip" data-mdb-placement="bottom" title="Add to favorites"></i>'
-            +'</button>'
-          +'</div>'
-        +'</div>'
-      )
+      $(".section-kids").append(generateCard(element))
     }
-    
-    element.colourCodes.forEach(color => {
-      $("."+element.id).find(".colourList").append($('<p class="color-dot" style="background-color:'+color+';"></p>'));
+    $( ".drag" ).draggable({
+      revert: true,
+      helper: () => {
+        return '<button type="button" class="btn btn-danger btn-floating dragged">'
+                +'<i class="fas fa-heart"></i>'
+              +'</button>';
+      }
     });
   });
 });
 
-fetch("shoes.json")
-.then(response => {
-   return response.json();
-})
-.then(data => {
-  data.shoes.forEach(element => {
-    
-  });
-});
+function generateCard(element){
+  return '<div class="card p-0 m-2" id="'+element.id+'">'
+          +'<div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">'
+            +'<img src="/img/'+element.id+'/1.jpg" class="card-img" />'
+            +'<a href="#!">'
+              +'<div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>'
+            +'</a>'
+          +'</div>'
+          +'<div class="card-header d-flex justify-content-around">'
+          +'<h5 class="card-title my-auto">'+element.name+'</h5>'
+              +'<div class="drag" id="'+element.id+'">'
+              +'<a class="btn btn-danger btn-floating">'
+                +'<i class="fas fa-heart"></i>'
+              +'</a></div>'
+            +'</div>'
+          +'<div class="card-body">'
+            +'<div class="colourList">'
+              +'<p class="color-dot" style="background-color:'+element.colourCodes[0]+';"></p>'
+              +'<p class="color-dot" style="background-color:'+element.colourCodes[1]+';"></p>'
+            +'</div>'
+            +'<p class="card-text p-0 m-0 lh-0">For : '+element.gender+'</p>'
+            +'<p class="card-text p-0 m-0 lh-0">Style : '+element.style+'</p>'
+            +'<p class="card-text">Price : $'+element.price+'</p>'
+        +'</div>';
+}
