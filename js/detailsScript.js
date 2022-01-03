966d4dbbb1cccc38c8ebd1a578d2d70762ee361f
Avishka Kavinda <45445179+acekavi@users.noreply.github.com>
2022-01-03 10:11:30 +0530

    Feat: Completeed all the core functionality

--- js/detailsScript.js
+++ js/detailsScript.js
@@ -61,6 +61,100 @@ $( function() {
 	});
 } );
 
-function handleDropEvent(){
-
+function handleDropEvent(event, ui){
+  var draggable = ui.draggable;
+  // alert( 'The square with ID "' + draggable.attr('id') + '" was dropped onto me!' );
+	addtoFavorites(draggable.attr('id'));
 }
+
+// List of the favorites
+let favList;
+
+// Function that runs after the window loaded completely
+$(window).bind("load", function() {
+  // conditional statement that checks if localStorage already exists
+  if (localStorage.getItem('favShoes')) {
+    favList = JSON.parse(localStorage.getItem('favShoes'));
+  } else {
+    favList = [];
+  }
+
+  refreshFavorites();
+  $("span.badge").html(`${favList.length}`);
+});
+
+//Adding to favorites
+function addtoFavorites(shoeID){
+  try {
+    const addingShoe = shoeID;
+    //Check whether its already in the list
+    if(favList.includes(addingShoe)){
+      console.log(`${addingShoe} is already in your favourites`);
+    }
+    // successfully adding a favorite
+    else{
+      favList.push(addingShoe);
+      $("span.badge").html(`${favList.length}`);
+
+      const newCard = allItems.filter(currentObj =>{
+        if(currentObj.id === addingShoe){
+          return currentObj;
+        }
+      });
+      
+      refreshFavorites();
+      localStorage.setItem("favShoes", JSON.stringify(favList));
+    }
+  } catch (e) {
+      if (e === QUOTA_EXCEEDED_ERR) {
+          console.log("Error: Local Storage limit exceeded");
+      } else {
+          console.log("Error: Saving to local storage");
+      }
+  }
+}
+
+function refreshFavorites(){
+  $("div.favoriteCards").html(``);
+
+  allItems.forEach(shoeObj => {
+    if(favList.includes(shoeObj.id)){
+      generateFavoriteCard(shoeObj);
+    }
+  });
+}
+
+// ------------------ Favorites Related functions ------------------
+function generateFavoriteCard(element){
+  $("div.favoriteCards").append(`
+  <div
+    class="bg-image card shadow-1-strong ms-4 mb-4"
+    style="background-image: url('${element.picture}'); height: 200px; width: 200px;"
+    >
+    <a href="${element.url}" class="text-light">
+      <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+        <div class="card-body">
+          <h6 class="card-title">${element.name}</h6>
+          <p class="card-text p-0 m-0 lh-0">Colour : ${element.colour}</p>
+          <p class="card-text p-0 m-0 lh-0">Style : ${element.style}</p>
+          <p class="card-text itemPrice fw-bold">Price : $${element.price}</p>
+          <a class="btn btn-danger mt-2 removeFav" id="${element.id}">Remove <i class="fas fa-trash"></i></a>
+        </div>
+      </div>  
+    </a>
+  </div>
+` );
+
+  $("a.removeFav").click(function () {
+    const thisShoe = ($(this).attr("id"));
+    
+    const indexOfItem = favList.indexOf(thisShoe);
+    if (indexOfItem > -1) {
+      favList.splice(indexOfItem, 1);
+      $("span.badge").html(`${favList.length}`);
+
+      $(this).closest(".card").remove();
+      localStorage.setItem("favShoes", JSON.stringify(favList));
+    }
+  });
+};
